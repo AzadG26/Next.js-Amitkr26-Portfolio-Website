93b6e08156f5305c672af1c9fa169b3cbd949f25
Azad Gupta <physicswallahlakshyabatch@gmail.com>
2024-10-22 12:46:10 +0530

    Update page.js

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -20,15 +20,13 @@ export default function Contact() {
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
-            summon the wizard
+            Connect with Amit Kumar
           </h1>
           <p className="text-center font-light text-sm xs:text-base">
-            Step into the circle of enchantment and weave your words into the
-            fabric of the cosmos. Whether you seek to conjure collaborations,
-            unlock mysteries, or simply share tales of adventure, your messages
-            are treasured scrolls within this realm. Use the form below to send
-            your missives through the ethereal network, and await the whisper of
-            magic in response.
+          Step into the realm of creativity and share your thoughts with Amit Kumar.
+          Whether you’re looking to collaborate on a project, explore new ideas in web development,
+          or simply share your journey, your messages are valued connections in this space.
+          Use the form below to reach out, and await Amit’s insightful response!
           </p>
         </div>
         <Form />
